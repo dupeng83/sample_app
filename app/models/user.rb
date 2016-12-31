4af1b7db1e5f3c71b6f8f53ec1420e49e628894b
dupeng <dupeng83@outlook.com>
2017-01-01 07:59:50 +0800

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -11,4 +13,10 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 end
---
+++ spec/features/creating_micropost_spec.rb
@@ -0,0 +1,12 @@
+# require "rails_helper"
+
+# RSpec.feature "Users can publish a micropost" do
+#   scenario "with valid content" do
+#     visit "/"
+#     # click_link "New Project"
+#     # fill_in "Name", with: "Sublime Text 3"
+#     # fill_in "Description", with: "A text editor for everyone"
+#     # click_button "Create Project"
+#     expect(page).to have_content "Sample App"
+#   end
+# end
